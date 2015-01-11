fce0036394cf7ed99e1a5b880f35f97dae3a16a1
ivvist <ivvist@gmail.com>
2015-01-12 00:35:00 +0400

    Fix for Error on app start

--- app_android/src/com/projectkaiser/app_android/AppAndroid.java
+++ app_android/src/com/projectkaiser/app_android/AppAndroid.java
@@ -1,9 +1,10 @@
 package com.projectkaiser.app_android;
 
 import org.apache.log4j.Level;
-
 import android.app.Application;
 import android.os.Environment;
+import java.io.File;
+import java.io.IOException;
 
 import com.projectkaiser.app_android.misc.ErrorReporter;
 
@@ -16,17 +17,32 @@ public class AppAndroid extends Application {
 	public void onCreate() {
 
         final LogConfigurator logConfigurator = new LogConfigurator();
-/*
-        logConfigurator.setFileName(Environment.getExternalStorageDirectory() + String.format("/Android/data/%s/files/",
-				getApplicationContext().getPackageName()) + "/pklog.txt");
+
+        File logdir = new File(getApplicationContext().getExternalFilesDir(null).getAbsolutePath());
+        if (!logdir.canWrite()){
+    		// TODO: write to log 
+        	System.out.println("Can't write.");		
+        }
+        if (!logdir.exists()){ 
+        	logdir.mkdirs();
+        }	
+        
+        File logfile = new File(logdir.getPath() + "/pklog.txt");
+        if (!logfile.exists()){try{
+    	       logfile.createNewFile();
+	       } catch (IOException e) {
+  		     e.printStackTrace();
+	    }}
+
+        logConfigurator.setFileName(logfile.getPath());
         logConfigurator.setRootLevel(Level.DEBUG);
-        // Set log level of a specific logger
+        // 	Set log level of a specific logger
         logConfigurator.setLevel("org.apache", Level.ERROR);
         logConfigurator.setFilePattern("%d - [%p::%c] - %m%n");
         logConfigurator.configure();
-*/		
+	
 		
-		ErrorReporter.bindReporter(getApplicationContext());
+    	ErrorReporter.bindReporter(getApplicationContext());
 		super.onCreate();
 	}
 
