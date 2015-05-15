1101ca7c535c4c9d2fbbecf4a5971bc32846afc4
ivvist <ivvist@gmail.com>
2015-05-15 17:34:38 +0400

    Versioning(2) http://projectkaiser.com/online/#!3077879

--- app_android/src/com/projectkaiser/app_android/jsonrpc/JsonRPC.java
+++ app_android/src/com/projectkaiser/app_android/jsonrpc/JsonRPC.java
@@ -5,6 +5,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.text.TextUtils;
 import com.projectkaiser.app_android.bl.obj.MNewComment;
 import com.projectkaiser.app_android.bl.obj.MRemoteNotSyncedIssue;
 import com.projectkaiser.app_android.jsonrpc.auth.AuthScheme;
@@ -18,22 +19,42 @@ import com.projectkaiser.app_android.rpc.IAppRPC;
 import com.projectkaiser.mobile.sync.MBasicRequest;
 import com.projectkaiser.mobile.sync.MCreateRequestEx;
 import com.projectkaiser.mobile.sync.MSynchronizeRequestEx;
+import com.projectkaiser.app_android.jsonrpc.errors.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.*;
 
 public class JsonRPC implements IAppRPC {
 
 	private final Logger log = Logger.getLogger(JsonRPC.class);
+
+	private final String SRV_FUNC_LOGIN = "login";
+	private final String SRV_FUNC_CREATE = "create";
+	private final String SRV_FUNC_SYNC = "sync";
+
+	private final Integer SRV_FUNC_LOGIN_VER = 1;
+	private final Integer SRV_FUNC_CREATE_VER = 1;
+	private final Integer SRV_FUNC_SYNC_VER = 1;
+
+	private HashMap<String, Integer> functionMap = new HashMap<String, Integer>();
+
+	public JsonRPC() {
+		functionMap.put(SRV_FUNC_LOGIN, SRV_FUNC_LOGIN_VER);
+		functionMap.put(SRV_FUNC_CREATE, SRV_FUNC_CREATE_VER);
+		functionMap.put(SRV_FUNC_SYNC, SRV_FUNC_SYNC_VER);
+	}
 
 	String getRpcUrl(String serverUrl) {
 		if (!serverUrl.endsWith("/"))
 			serverUrl += "/";
 		return serverUrl + "soap";
 	}
-	
+
 	private JsonObjectBuilder getComments(MCreateRequestEx request) {
 
 		JsonObjectBuilder jb = new JsonObjectBuilder(false);
 		jb.openArray();
-		for (MNewComment c: request.getNewComments()) {
+		for (MNewComment c : request.getNewComments()) {
 			jb.openObject();
 			jb.addProp("id", c.getId());
 			jb.addProp("ns", c.isNonSyncedTask());
@@ -43,114 +64,120 @@ public class JsonRPC implements IAppRPC {
 		}
 		jb.closeArrayProp();
 		return jb;
-		
 	}
-	
+
 	private JsonObjectBuilder getTasks(MCreateRequestEx request) {
 
 		JsonObjectBuilder jb = new JsonObjectBuilder(false);
 		jb.openArray();
-		for (MRemoteNotSyncedIssue i: request.getNewIssues()) {
-			
+		for (MRemoteNotSyncedIssue i : request.getNewIssues()) {
+
 			jb.openObject();
 			jb.addProp("id", i.getId());
 			jb.addProp("name", i.getName());
-			
-			if (i.getDescription()!=null)
+
+			if (i.getDescription() != null)
 				jb.addProp("des", i.getDescription());
-			if (i.getAssigneeId()!=null && i.getAssigneeId().longValue()>0)
+			if (i.getAssigneeId() != null && i.getAssigneeId().longValue() > 0)
 				jb.addProp("aId", i.getAssigneeId());
-			if (i.getResponsibleId()!=null && i.getResponsibleId().longValue()>0) 
+			if (i.getResponsibleId() != null
+					&& i.getResponsibleId().longValue() > 0)
 				jb.addProp("rId", i.getResponsibleId());
-			if (i.getBudget()!=null && i.getBudget().intValue()!=0)
+			if (i.getBudget() != null && i.getBudget().intValue() != 0)
 				jb.addProp("bud", i.getBudget());
-			if (i.getDueDate()!=null && i.getDueDate().longValue()>0)
+			if (i.getDueDate() != null && i.getDueDate().longValue() > 0)
 				jb.addProp("due", i.getDueDate());
-			if (i.getPriority()!=null && i.getPriority().longValue()>0) 
+			if (i.getPriority() != null && i.getPriority().longValue() > 0)
 				jb.addProp("pri", i.getPriority());
 
 			jb.addProp("sta", i.getState());
-			jb.addProp("fId", i.getFolderId());			
+			jb.addProp("fId", i.getFolderId());
 			jb.closeObject();
 		}
 		jb.closeArrayProp();
 		return jb;
-		
+
 	}
 
 	@Override
 	public String create(MCreateRequestEx request) {
-		
-		return request(request, 
-				"create", 
-				getTasks(request), 
-				getComments(request), 
-				request.getLocale().getLanguage());
-		
+
+		return request(request, GetFuncRequest("create", SRV_FUNC_CREATE_VER),
+				getTasks(request), getComments(request), request.getLocale()
+						.getLanguage());
+
 	}
-	
+
 	private String request(MBasicRequest request, Object... args) {
-		JsonObjectBuilder j = new JsonObjectBuilder(false); 
-		
+		JsonObjectBuilder j = new JsonObjectBuilder(false);
+
 		j.openObject();
 		j.addProp("jsonrpc", "2.0");
 		j.addProp("method", "doCollectionView");
-		
+
 		j.openArrayProp("params");
 		j.openObject();
-		
+
 		// <scheme>
 		j.openObjectProp("scheme");
 		AuthScheme scheme = request.getAuthScheme();
 		if (scheme instanceof PlainAuthScheme) {
 			j.addProp("type", "PlainAuthScheme");
-			j.addProp("userName", ((PlainAuthScheme)scheme).getUserName());
-			j.addProp("password", ((PlainAuthScheme)scheme).getPassword());
+			j.addProp("userName", ((PlainAuthScheme) scheme).getUserName());
+			j.addProp("password", ((PlainAuthScheme) scheme).getPassword());
 		} else if (scheme instanceof SessionAuthScheme) {
 			j.addProp("type", "SessionAuthScheme");
-			j.addProp("session", ((SessionAuthScheme)scheme).getSessionId());
+			j.addProp("session", ((SessionAuthScheme) scheme).getSessionId());
 		} else if (scheme instanceof GoogleAuthScheme) {
 			j.addProp("type", "GoogleTokenScheme");
-			j.addProp("token", ((GoogleAuthScheme)scheme).getToken());
-			j.addProp("email", ((GoogleAuthScheme)scheme).getEmail());
-			j.addProp("displayName", ((GoogleAuthScheme)scheme).getDisplayName());
-			j.addProp("pictureUrl", ((GoogleAuthScheme)scheme).getPictureUrl());
-		};
+			j.addProp("token", ((GoogleAuthScheme) scheme).getToken());
+			j.addProp("email", ((GoogleAuthScheme) scheme).getEmail());
+			j.addProp("displayName",
+					((GoogleAuthScheme) scheme).getDisplayName());
+			j.addProp("pictureUrl", ((GoogleAuthScheme) scheme).getPictureUrl());
+		}
+		;
 		j.closeObject();
 		// </scheme>
-		
+
 		// <headers>
 		j.openArrayProp("headers");
+		j.openObject();
+		j.addProp("key", "intf");
+		j.addProp("value", "android:1");
+		j.closeObject();
 		j.closeArrayProp();
 		// </headers>
-		
+
 		// <args>
 		j.openArrayProp("args");
-		for (Object arg:args) {
+		for (Object arg : args) {
 			if (arg instanceof String)
-				j.addString((String)arg);
+				j.addString((String) arg);
 			else if (arg instanceof JsonObjectBuilder)
-				j.addEscapedJson((JsonObjectBuilder)arg);
+				j.addEscapedJson((JsonObjectBuilder) arg);
 			else
-				throw new RuntimeException("Not supported class "+arg.getClass().getName()+" at JsonRPC.request");
+				throw new RuntimeException("Not supported class "
+						+ arg.getClass().getName() + " at JsonRPC.request");
 		}
 		j.closeArrayProp();
 		// </args>
-		
-		j.addProp("target", "com.triniforce.server.plugins.webclient.CDMobileClient");
+
+		j.addProp("target",
+				"com.triniforce.server.plugins.webclient.CDMobileClient");
 		j.addProp("parentOf", 0L);
-		
+
 		// <columns>
 		j.openArrayProp("columns");
 		j.addString("result");
 		j.closeArrayProp();
 		// </columns>
-		
+
 		// <where>
 		j.openArrayProp("where");
 		j.closeArrayProp();
 		// </where>
-		
+
 		// <orderBy>
 		j.openArrayProp("orderBy");
 		j.closeArrayProp();
@@ -171,10 +198,11 @@ public class JsonRPC implements IAppRPC {
 		j.closeObject();
 		j.closeArrayProp();
 		j.closeObject();
-		
+
 		try {
-			JSONObject o = JsonClient.makeRequest(getRpcUrl(request.getServerUrl()), j);
-	
+			JSONObject o = JsonClient.makeRequest(
+					getRpcUrl(request.getServerUrl()), j);
+
 			if (!o.isNull("error")) {
 				JSONObject error = o.getJSONObject("error");
 				String message;
@@ -186,51 +214,142 @@ public class JsonRPC implements IAppRPC {
 				if (error.isNull("stackTrace"))
 					stack = "unknown";
 				else
-					stack = error.getString("stackTrace");						
+					stack = error.getString("stackTrace");
 				if (stack.contains(".EAuth$"))
 					throw new EAuthError();
 				throw new EAppException(message);
-			} 		
+			}
 
 			if (!o.isNull("result")) {
 				JSONObject result = o.getJSONObject("result");
+
+				validateServer(result);
+
 				if (!result.isNull("values")) {
 					JSONArray values = result.getJSONArray("values");
-					if (values.length()<1)
-						throw new EJsonException("Unexpected JSON format: no values returned");
+					if (values.length() < 1)
+						throw new EJsonException(
+								"Unexpected JSON format: no values returned");
 					return values.getString(0);
-				} else				
-					throw new EJsonException("Unexpected JSON format: values not found");
+				} else
+					throw new EJsonException(
+							"Unexpected JSON format: values not found");
 			} else
-				throw new EJsonException("Unexpected JSON format: result not found");
-			
+				throw new EJsonException(
+						"Unexpected JSON format: result not found");
+
 		} catch (JSONException e) {
-			log.error( "Unable to parse json", e);
+			log.error("Unable to parse json", e);
 			throw new EJsonException(e);
+		}
+	}
+
+	private void validateServer(JSONObject result) {
+
+		boolean bAppFneFound = false;
+		boolean bAppFnOut = false;
+		boolean bSrvFnOut = false;
+		String errfnName = "";
+
+		if (!result.isNull("headers")) {
+			try {
+				JSONObject headers = result.getJSONObject("headers");
+				String strFunc = headers.getString("intf");
+				TextUtils.SimpleStringSplitter splitter = new TextUtils.SimpleStringSplitter(
+						',');
+				splitter.setString(strFunc);
+				TextUtils.SimpleStringSplitter fnSplitter = new TextUtils.SimpleStringSplitter(
+						':');
+
+				HashMap<String, Integer> tempMap = (HashMap<String, Integer>) functionMap.clone();
+				while (splitter.hasNext()) {
+					String fn = splitter.next();
+					bAppFneFound = false;
+					errfnName = "";
+					Iterator<Entry<String, Integer>> it = tempMap
+							.entrySet().iterator();
+					while (it.hasNext()) {
+						HashMap.Entry<String, Integer> fnItem = (HashMap.Entry<String, Integer>) it
+								.next();
+						if (fn.contains(fnItem.getKey())) {
+							bAppFneFound = true;
+							fnSplitter.setString(fn);
+							int idx = 0;
+							while (splitter.hasNext()) {
+								String srvfn = splitter.next();
+								if (idx == 1) {
+									if (Integer.parseInt(srvfn) > fnItem
+											.getValue()) {
+										bAppFnOut = true;
+										break;
+									} else if (Integer.parseInt(srvfn) < fnItem
+											.getValue()) {
+										bSrvFnOut = true;
+										break;
+									}
+
+								}
+								idx = idx + 1;
+							}
+							tempMap.remove(fnItem.getKey());
+						}
+					}
+					if (!bAppFneFound) {
+						errfnName = fn;
+						break;
+					}
+				}
+				if (tempMap.keySet().size()>0){ // Application has more functions than Server
+					throw new EServerOutDate("Server is outdated: one or more functions not found on Server");
+				}
+				if (!bAppFneFound) {
+					throw new EServerOutDate("Application is outdated: Function '"
+							+ errfnName + "' not found in Application");
+				}
+				if (!bAppFnOut) {
+					throw new EServerOutDate(
+							"Application is outdated. Please update your application");
+				}
+				if (!bSrvFnOut) {
+					throw new EServerOutDate(
+							"Your Server is outdated. Please update your Server");
+				}
+
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				throw new EJsonException(
+						"Unexpected JSON format: headers not found");
+			}
+
 		}
 	}
 
 	@Override
 	public String syncronize(MSynchronizeRequestEx request) {
 
-		return request(request, 
-				"synchronize", 
+		return request(
+				request,
+				GetFuncRequest("sync", SRV_FUNC_SYNC_VER),
 				request.getLocale().getLanguage(),
-				request.getWorkingSetsDigest()==null?"":request.getWorkingSetsDigest(),
-				request.getIssuesDigest()==null?"":request.getIssuesDigest(),
-				request.getProjectsDigest()==null?"":request.getProjectsDigest()
-						);
-		
+				request.getWorkingSetsDigest() == null ? "" : request
+						.getWorkingSetsDigest(),
+				request.getIssuesDigest() == null ? "" : request
+						.getIssuesDigest(),
+				request.getProjectsDigest() == null ? "" : request
+						.getProjectsDigest());
+
 	}
-	
+
 	@Override
 	public String login(MBasicRequest request) {
 
-		return request(request, 
-				"login", 
-				request.getLocale().getLanguage()
-				);
-		
+		return request(request, GetFuncRequest("login", SRV_FUNC_LOGIN_VER),
+				request.getLocale().getLanguage());
+
+	}
+
+	private String GetFuncRequest(String fname, int version) {
+		return fname + ":" + version;
 	}
 
 }
---
+++ app_android/src/com/projectkaiser/app_android/jsonrpc/errors/EServerOutDate.java
@@ -0,0 +1,13 @@
+package com.projectkaiser.app_android.jsonrpc.errors;
+
+public class EServerOutDate extends RuntimeException {
+	public EServerOutDate() {
+		super();
+	}
+
+	public EServerOutDate(String msg) {
+		super(msg);
+	}
+
+	
+}
